8076e8fcf1280b57416d592d2ca69e65c857b67b
Matthew Yu <matthewwangyu@gmail.com>
2024-11-04 00:47:24 -0500

    feat: added new modle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Validations
-  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+  validates :name, presence: true, length: {minimum: 2, maximum: 50}
+  validates :email, presence: true, uniqueness: true, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :password, presence: true, length: {minimum: 6}, if: :password_required?
 
   has_secure_password
 
