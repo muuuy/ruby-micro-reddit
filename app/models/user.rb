0df05bba28c8cf3a117932533e70f69ad329c391
Matthew Yu <matthewwangyu@gmail.com>
2024-11-04 00:25:35 -0500

    feat: added user model

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Validations
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_required?
+
+  has_secure_password
+
+  private
+
+  # Ensure password validation is required only on create or when changing the password
+  def password_required?
+    new_record? || !password.nil?
+  end
+end
